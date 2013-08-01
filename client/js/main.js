375f6a2fcdc041e34b79c1c27f0a54642d705419
Alan Shaw <alan@freestyle-developments.co.uk>
2013-08-01 16:03:09 +0100

    Follow ship, use different ship icon per voyage

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,6 @@
 Meteor.startup(function () {
 
-  var map = L.map('map').setView([51.505, -0.09], 3)
+  var map = L.map('map').setView([51.505, -0.09], 5)
 
   new L.StamenTileLayer("watercolor").addTo(map)
 
@@ -8,6 +8,7 @@ Meteor.startup(function () {
 
   var positions = Positions.findByTimestampAsc()
     , marker = null
+    , ships = ["container", "ferry", "fishing"]
 
   positions.observe({
     added: function (pos) {
@@ -16,11 +17,7 @@ Meteor.startup(function () {
 
       if (!marker) {
 
-        var icon = L.icon({
-          iconUrl: 'img/icon-ship-container.png',
-          iconSize: [60, 60],
-          iconAnchor: [30, 30]
-        })
+        var icon = L.icon({iconUrl: "img/icon-ship-"+ships[Math.floor(Math.random()*ships.length)]+".png", iconSize: [60, 60], iconAnchor: [30, 30]})
 
         return marker = L.marker(latlng, {icon: icon, iconAngle: pos.heading}).addTo(map)
       }
@@ -28,7 +25,15 @@ Meteor.startup(function () {
       // Move marker to new position
       marker.setLatLng(latlng).setIconAngle(pos.heading)
 
+      // Move map view
+      map.setView(latlng, map.getZoom())
+
       Meteor.setTimeout(drawTracks, 500)
+    },
+    // When start a new voyage, remove marker so we get a new ship graphic
+    removed: function () {
+      map.removeLayer(marker)
+      marker = null
     }
   })
 
